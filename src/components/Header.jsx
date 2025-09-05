57a533981c41e38b1933c17bdac82b8d82439c66
Vijay Goswami <vijaygoswami@attrixtech.com>
2025-09-05 13:12:39 +0530

    logo updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,15 +8,15 @@ export default function Header() {
 
     return (
         <header className="bg-white dark:bg-gray-800 shadow-md sticky top-0 z-50">
-            <div className="container mx-auto flex justify-between items-center p-4">
+            <div className="container mx-auto flex justify-between items-center px-4 py-2">
                 {/* Logo / Brand */}
-                <Link href="/">
-  <img
-    src="/logo.png"
-    alt="MGSG Construction Logo"
-    style={{ height: '100px', width: 'auto' }}
-  />
-</Link>
+                <Link href="/" className="dark:text-white">
+                    <img
+                        src="/logo.svg"
+                        alt="MGSG Construction Logo"
+                        className="h-[100px] w-auto"
+                    />
+                </Link>
 
 
                 {/* Desktop Navigation */}
